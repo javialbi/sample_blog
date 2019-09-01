a0660941ccc1dab23c04dd6f03c28eb0fc604f5f
Javier Alberty <javiermalberty@gmail.com>
2019-09-01 11:58:33 -0500

    Basic crud functionality completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'posts#index', as: 'home'
-  get 'about' => 'pages#about'
-  resources :posts
+  get 'about' => 'pages#about', as: 'about'
+  resources :posts do
+    resources :comments
+  end
 end
